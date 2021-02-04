de7be884f0de857bd5d940d3f2cf2c1e01594a23
EpicDoomSir <eitands@gmail.com>
2021-02-04 06:50:23 -0500

    started adding components

--- src/App.js
+++ src/App.js
@@ -1,27 +1,20 @@
 import './App.css';
-import {URL} from './index'
-import { Route, Switch } from 'react-router-dom';
-import { connect } from 'react-redux'
+import React from 'react';
+import FamilyContainer from './Containers/FamilyContainer';
+// import {URL} from './index'
+// import { Route, Switch } from 'react-router-dom';
+// import { connect } from 'react-redux'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+  render(){
+    return (
+      <div className="App">
+        <h1>hello from app</h1>
+        <FamilyContainer />
+      </div>
+    )
+  }
 }
 
 export default App;
---
+++ src/Containers/FamilyContainer.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+class FamilyContainer extends React.Component {
+
+    render(){
+        return(
+            <>
+                <h2>family container</h2>
+            </>
+        )
+    }
+}
+
+export default FamilyContainer
