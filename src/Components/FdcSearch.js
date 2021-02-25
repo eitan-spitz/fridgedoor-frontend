c4a442e9dc8aea6ad633b13edb45aa6e05d55cea
EpicDoomSir <eitands@gmail.com>
2021-02-25 10:29:58 -0500

    lifted state out of fdcsearch

--- src/Components/FdcSearch.js
+++ src/Components/FdcSearch.js
@@ -2,58 +2,28 @@ import React from 'react'
 
 class FdcSearch extends React.Component {
 
-    state = {
-        query: '',
-        apiResponse: null,
-        fetched: false
-    }
-
-    fdcData = {
-        url: "https://api.nal.usda.gov/fdc/v1/foods/search?",
-        apiKey: "CS1Rfy9P2MqeJeHkcNnhBjhyMBJ9d6G5cUSdkOAu",
-        pageSize: 10
-      }
-      
     componentDidUpdate(){
-        if(!this.state.fetched){
-            if(this.state.query.length >= 4 ){
+        if(!this.props.fetched){
+            if(this.props.query.length >= 4 ){
                 console.log('length over 4')
-                this.searchFetch()
+                this.props.searchFetch()
             }
         }
     }
 
-    changeHandler = (e) => {
-        console.log('working')
-        this.setState({ [e.target.name]: e.target.value, fetched: false})
-    }
-    
-    searchFetch = () => {
-        console.log('in fetch')
-        fetch(`${this.fdcData.url}api_key=${this.fdcData.apiKey}&query=${this.state.query}&pageSize=${this.fdcData.pageSize}`, {
-            method: "GET",
-            headers: {
-                "Accept": "application/json",
-                "Content-type": "application/json"
-            }
-        })
-        .then(r => r.json())
-        .then(returnedData => {
-            console.log(returnedData)
-            this.setState({apiResponse: returnedData.foods, fetched: true})
-        })
+    localChangeHandler = (e) => {
+        this.props.changeHandler(e)
     }
     
     localSubmitHandler = (e) => {
         e.preventDefault()
-
     }
 
     render(){
         return(
             <>
             <form onSubmit={this.localSubmitHandler} className="form" >
-                <input type="text" name="query" placeholder="Search Food Database" value={this.state.query} onChange={this.changeHandler} />
+                <input type="text" name="query" placeholder="Search Food Database" value={this.props.query} onChange={this.localChangeHandler} />
             </form>
             </>
         )
--- src/Containers/SearchContainer.js
+++ src/Containers/SearchContainer.js
@@ -3,13 +3,44 @@ import FdcSearch from '../Components/FdcSearch'
 
 class SearchContainer extends React.Component {
 
+    state = {
+        query: '',
+        apiResponse: null,
+        fetched: false
+    }
+
+    fdcData = {
+        url: "https://api.nal.usda.gov/fdc/v1/foods/search?",
+        apiKey: "CS1Rfy9P2MqeJeHkcNnhBjhyMBJ9d6G5cUSdkOAu",
+        pageSize: 10
+      }
+
+    changeHandler = (e)=> {
+        console.log('working')
+        this.setState({ [e.target.name]: e.target.value, fetched: false})
+    }
+    searchFetch = () => {
+        console.log('in fetch')
+        fetch(`${this.fdcData.url}api_key=${this.fdcData.apiKey}&query=${this.state.query}&pageSize=${this.fdcData.pageSize}`, {
+            method: "GET",
+            headers: {
+                "Accept": "application/json",
+                "Content-type": "application/json"
+            }
+        })
+        .then(r => r.json())
+        .then(returnedData => {
+            console.log(returnedData)
+            this.setState({apiResponse: returnedData.foods, fetched: true})
+        })
+    }
 
     render(){
         console.log(this.props)
         return(
             <>
-                <FdcSearch/>
                 <button onClick={this.props.history.goBack}>go back</button>
+                <FdcSearch query={this.state.query} fetched={this.state.fetched} changeHandler={this.changeHandler} searchFetch={this.searchFetch} />
                 
             </>
         )
