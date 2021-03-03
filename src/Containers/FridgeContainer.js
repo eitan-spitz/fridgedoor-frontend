6a588f22bfe31c44de1505a51a9ce4334e9e5176
EpicDoomSir <eitands@gmail.com>
2021-03-03 07:48:09 -0500

    items displayed correctly once adding and deleting

--- src/Containers/FridgeContainer.js
+++ src/Containers/FridgeContainer.js
@@ -7,24 +7,29 @@ import SearchContainer from './SearchContainer'
 class FridgeContainer extends React.Component {
 
     state = {
-        items: []
+        items: [],
+        fridgeItems: []
+    }
+
+    componentDidMount() {
+        this.setState({ items: this.props.fridge.items, fridgeItems: this.props.fridge.fridge_items })
     }
 
     arrayOfItems = () => {
-        return this.props.fridge.items.map(item => <FridgeItem item={item} deleteItem={this.deleteItem} key={item.id} />)
+        return this.state.items.map(item => <FridgeItem item={item} deleteItem={this.deleteItem} key={item.id} />)
     }
 
     deleteItem = (itemObj) => {
-        let fridgeItemToDelete = this.props.fridge.fridge_items.find(fridgeItem => fridgeItem.item_id === itemObj.id)
-        fetch(`http://localhost:3000/families/${this.props.familyId}/fridges/${this.props.fridge.id}/fridge_items/${fridgeItemToDelete.id}`,{
+        let fridgeItemToDelete = this.state.fridgeItems.find(fridgeItem => fridgeItem.item_id === itemObj.id)
+        fetch(`http://localhost:3000/families/${this.props.familyId}/fridges/${this.props.fridge.id}/fridge_items/${fridgeItemToDelete.id}`, {
             method: "DELETE",
             headers: {
                 "Accepts": "application/json",
                 "Content-type": "application/json",
             }
         })
-        .then(r=>r.json())
-        .then(resp => console.log(resp))
+            .then(r => r.json())
+            .then(resp => console.log(resp))
     }
 
     addItem = (itemObj) => {
@@ -35,17 +40,18 @@ class FridgeContainer extends React.Component {
                 "Accept": "application/json",
                 "Content-type": "application/json"
             },
-            body: JSON.stringify({fdc_id: itemObj.fdcId, fdc_description: itemObj.description, data_type: itemObj.dataType})
+            body: JSON.stringify({ fdc_id: itemObj.fdcId, fdc_description: itemObj.description, data_type: itemObj.dataType })
         })
-        .then(r=>r.json())
-        .then(item => {
-            console.log(item)
-            this.createFridgeItem(item)
-            .then(r=>r.json())
-            .then(fridgeItem => {
-                console.log(fridgeItem)
+            .then(r => r.json())
+            .then(returnedItem => {
+                console.log(returnedItem)
+                this.createFridgeItem(returnedItem)
+                    .then(r => r.json())
+                    .then(returnedFridgeItem => {
+                        console.log(returnedFridgeItem)
+                        this.setState({ items: [...this.state.items, returnedItem], fridgeItems: [...this.state.fridgeItems, returnedFridgeItem] })
+                    })
             })
-        })
     }
 
     createFridgeItem = (itemObj) => {
@@ -55,37 +61,37 @@ class FridgeContainer extends React.Component {
                 "Accept": "application/json",
                 "Content-type": "application/json"
             },
-            body: JSON.stringify({fridge_id: this.props.fridge.id, item_id: itemObj.id})
+            body: JSON.stringify({ fridge_id: this.props.fridge.id, item_id: itemObj.id })
         })
     }
 
-    render(){
-        return(
+    render() {
+        return (
             <>
-            <Switch>
-                <Route path='/families/fridges/:id' render={()=> {
-                    return(
-                        <div className="container">
-                            <div className="fridge">
-                            <h2>{this.props.fridge.name}</h2>
-                                {this.arrayOfItems()}
+                <Switch>
+                    <Route path='/families/fridges/:id' render={() => {
+                        return (
+                            <div className="container">
+                                <div className="fridge">
+                                    <h2>{this.props.fridge.name}</h2>
+                                    {this.arrayOfItems()}
+                                </div>
+                                <div className="search">
+                                    <SearchContainer addItem={this.addItem} />
+                                </div>
                             </div>
-                            <div className="search">
-                                <SearchContainer addItem={this.addItem} />
-                            </div>
-                        </div>
-                    )
-                }} />
-            </Switch>
+                        )
+                    }} />
+                </Switch>
             </>
         )
     }
 }
 
-function msp(state){
-    return{
+function msp(state) {
+    return {
         user: state.user
     }
 }
 
-export default connect(msp, null) (FridgeContainer)
+export default connect(msp, null)(FridgeContainer)
