4682e7c9096f0ce734a55a5ecfdfec47ba9c2e0e
EpicDoomSir <eitands@gmail.com>
2021-05-16 09:00:53 -0400

    cleaned up comments

--- src/Components/ShoppingCard.js
+++ src/Components/ShoppingCard.js
@@ -4,7 +4,6 @@ import { NavLink } from 'react-router-dom'
 function ShoppingCard(props){
     const shoppinglist = props.shoppinglist
     return (
-        // important detail, props hold family id as well so will come in handy when refactoring pathway
         <div className="shopping-card">
             <h3>{shoppinglist.name}</h3>
             <NavLink to={`/families/shopping/${shoppinglist.id}`} className="link" ><button>Open</button></NavLink> 
