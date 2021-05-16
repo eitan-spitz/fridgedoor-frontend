770aa623f7fa0e870b401086b2a2ec993a3f0995
EpicDoomSir <eitands@gmail.com>
2021-05-16 08:59:00 -0400

    corrected about

--- src/App.js
+++ src/App.js
@@ -42,7 +42,7 @@ class App extends React.Component {
                   <h3>Let’s open the FridgeDoor.</h3>
                   <h4>FridgeDoor is a fridge management app that allows families to have a centralized database for fridge management.</h4>
                   <h3>Designed to ease your mind.</h3>
-                  <h4>Whether you’re out shopping or contemplating getting off the couch for a snack, you’ll always know exactly what’s waiting for you. Don’t worry about whether or not you’re missing something important for that recipe you’re thinking of trying-- we’ve got you covered.</h4>
+                  <h4>Whether you’re out shopping or contemplating getting off the couch for a snack, you’ll always know exactly what’s waiting for you. Don’t worry about whether or not you’re missing something important for that recipe you’re thinking of trying -- we’ve got you covered.</h4>
                   <h3>Fridge(s) for the whole family.</h3>
                   <h4>Wondering what’s stored in the deep-freezer downstairs? We’ll handle that too -- FridgeDoor allows for multiple fridges to be managed from one account, and that account can be accessed by the whole family (or your roommates, if you trust them).</h4>
                 </div>
